040f93314b86914937fe7325b93f7f2f82908557
CamilleGvd <camille_gavardon@hotmail.fr>
2019-09-04 16:35:28 +0700

    modif bis

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect("mongodb://localhost/expense", {
-  useNewUrlParser: true
-});
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  });
+} else {
+  mongoose.connect("http://localhost:3000/", {
+    useNewUrlParser: true
+  });
+}
 
 const Expense = mongoose.model("Expense", {
   user: String,
@@ -29,9 +35,15 @@ app.post("/", async (req, res) => {
       description: req.body.description,
       amount: req.body.amount
     });
-    expenseReport.push(newExpense);
     await newExpense.save();
-    res.json({ message: "Expense created!" });
+    return res.json({
+      user: newExpense.user,
+      description: newExpense.description,
+      amount: newExpense.amount
+    });
+    // expenseReport.push(newExpense);
+    // await newExpense.save();
+    // res.json({ message: "Expense created!" });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -42,9 +54,9 @@ app.post("/", async (req, res) => {
 app.get("/", async (req, res) => {
   try {
     const expenses = await Expense.find();
-    res.json(expenses);
+    return res.json(expenses);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    return res.status(400).json({ error: error.message });
   }
 });
 
