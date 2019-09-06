2aa8a562e2e127ea62cc339b7e5c6bfdf0d15dd5
CamilleGvd <camille_gavardon@hotmail.fr>
2019-09-06 17:10:38 +0800

    delete reviews

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ const Expense = mongoose.model("Expense", {
 
 const expenseReport = [];
 
-/* Create - Read - Update - Delete */
-
-/* 1- Create an expense report */
+/*  Create an expense report */
 app.post("/", async (req, res) => {
   try {
     const newExpense = new Expense({
@@ -41,15 +39,12 @@ app.post("/", async (req, res) => {
       description: newExpense.description,
       amount: newExpense.amount
     });
-    // expenseReport.push(newExpense);
-    // await newExpense.save();
-    // res.json({ message: "Expense created!" });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
-/* 2- Read : expense report list */
+/* Read : expense report list */
 
 app.get("/", async (req, res) => {
   try {
@@ -59,20 +54,6 @@ app.get("/", async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 });
-
-/* 3- Update : total per user */
-
-/* Ideas : find by ... Front-end ?*/
-// app.get("/total", async (req, res) => {
-//   try {
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
-
-// app.listen(3000, () => {
-//   console.log("Server started");
-// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
